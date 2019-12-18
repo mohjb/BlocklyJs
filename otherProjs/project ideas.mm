8c078a71699440b6a12ecb6ccba0a76505455562
mohjb <mohamadjb@gmail.com>
2019-12-18 15:52:33 +0300

    demo

---
+++ otherProjs/project ideas.mm
@@ -0,0 +1,254 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<attribute_registry SHOW_ATTRIBUTES="selected"/>
+<node CREATED="1576670252833" ID="ID_1284995857" MODIFIED="1576672268091" TEXT="project ideas">
+<node CREATED="1576670265638" HGAP="-17" ID="ID_1213379568" MODIFIED="1576673119249" POSITION="right" TEXT="games" VSHIFT="-7">
+<node CREATED="1576671326522" HGAP="24" ID="ID_860224745" MODIFIED="1576673174578" TEXT="Twintris" VSHIFT="58"/>
+<node CREATED="1576671336664" HGAP="27" ID="ID_1244566821" MODIFIED="1576673243273" TEXT="Rainbow" VSHIFT="-3">
+<node CREATED="1576671343924" ID="ID_559341886" MODIFIED="1576671894535" TEXT="fman editors">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Rainbow fman editors
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1576671894510" ID="ID_1979953070" MODIFIED="1576671901497" TEXT="map ed"/>
+<node CREATED="1576671901906" ID="ID_899631927" MODIFIED="1576671910226" TEXT="scenes relational-map"/>
+<node CREATED="1576671910795" ID="ID_141132074" MODIFIED="1576671913554" TEXT="assets management"/>
+</node>
+</node>
+<node CREATED="1576671409804" HGAP="28" ID="ID_365922590" MODIFIED="1576673289424" TEXT="Spiderball" VSHIFT="3"/>
+<node CREATED="1576671414144" HGAP="28" ID="ID_1275323852" MODIFIED="1576673278778" TEXT="3d games" VSHIFT="7">
+<node CREATED="1576671421465" HGAP="14" ID="ID_53036128" MODIFIED="1576673270974" TEXT="rts" VSHIFT="5"/>
+<node CREATED="1576671441974" HGAP="16" ID="ID_1339296641" MODIFIED="1576673268881" VSHIFT="5">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      tree branchs
+    </p>
+    <p>
+      walking and building
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1576671463692" HGAP="21" ID="ID_1371143931" MODIFIED="1576673265363" VSHIFT="5">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      space pipes\
+    </p>
+    <p>
+      paths walking
+    </p>
+    <p>
+      and building
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1576671486156" HGAP="21" ID="ID_868337609" MODIFIED="1576673259624" TEXT="tower defense" VSHIFT="11">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      kuwait areas edition
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1576672648205" HGAP="17" ID="ID_1076007884" MODIFIED="1576673262204" TEXT="shooter" VSHIFT="-80">
+<node CREATED="1576672622952" HGAP="22" ID="ID_1054067964" MODIFIED="1576672666312" TEXT="last man standing" VSHIFT="25"/>
+<node CREATED="1576671427567" HGAP="14" ID="ID_1856474205" MODIFIED="1576672726568" TEXT="Capture the flag" VSHIFT="1"/>
+<node CREATED="1576672693303" HGAP="7" ID="ID_403080801" MODIFIED="1576673350573" TEXT="tag-team" VSHIFT="-11"/>
+</node>
+</node>
+<node CREATED="1576671519214" HGAP="26" ID="ID_579841859" MODIFIED="1576673239136" TEXT="cards" VSHIFT="2">
+<node CREATED="1576671524736" ID="ID_770802407" MODIFIED="1576671528917" TEXT="Uno"/>
+<node CREATED="1576671529238" ID="ID_621591381" MODIFIED="1576671535419" TEXT="kout bo 6"/>
+</node>
+<node CREATED="1576671538893" ID="ID_982850254" MODIFIED="1576671543844" TEXT="Educa games"/>
+<node CREATED="1576671545921" ID="ID_155446199" MODIFIED="1576673233691" TEXT="Business gamification" VSHIFT="5"/>
+<node CREATED="1576671562353" HGAP="21" ID="ID_1888401655" MODIFIED="1576673232082" TEXT="School gamification" VSHIFT="7"/>
+<node CREATED="1576671569194" HGAP="24" ID="ID_691219523" MODIFIED="1576673236889" TEXT="Music editor" VSHIFT="3">
+<node CREATED="1576671585388" HGAP="17" ID="ID_1682718658" MODIFIED="1576673067152" TEXT="Beat gamification" VSHIFT="17">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Music-game-engine
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1576672541832" HGAP="22" ID="ID_83243315" MODIFIED="1576673227559" TEXT="Sakuban" VSHIFT="-47"/>
+</node>
+<node COLOR="#006633" CREATED="1576670273866" HGAP="-34" ID="ID_1685322874" MODIFIED="1576672319793" POSITION="left" TEXT="portlet" VSHIFT="-24">
+<node CREATED="1576670323231" ID="ID_1377394892" MODIFIED="1576670351663" TEXT="web editor webapp">
+<node CREATED="1576670351665" ID="ID_888959978" MODIFIED="1576670365892" TEXT="Dawaer"/>
+<node CREATED="1576670366387" ID="ID_1003325074" MODIFIED="1576670399368" TEXT="palette">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      wysiwyg
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1576670407538" ID="ID_1690315610" MODIFIED="1576670417574" TEXT="scaffolding"/>
+<node CREATED="1576670418113" ID="ID_582445076" MODIFIED="1576671625461" TEXT="CanDo">
+<node CREATED="1576670427740" ID="ID_1050321522" MODIFIED="1576670432188" TEXT="web"/>
+<node CREATED="1576670432892" ID="ID_966707480" MODIFIED="1576670443105" TEXT="ThreeJs">
+<node CREATED="1576670443669" HGAP="17" ID="ID_666162965" MODIFIED="1576673313240" VSHIFT="18">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ThreeJs
+    </p>
+    <p>
+      environment
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1576670470420" ID="ID_1521995732" MODIFIED="1576670476964" TEXT="canvas"/>
+<node CREATED="1576670478147" ID="ID_1395585048" MODIFIED="1576670486576" TEXT="storage">
+<node CREATED="1576670486858" HGAP="12" ID="ID_1548750083" MODIFIED="1576671804825" TEXT="multi-user" VSHIFT="24"/>
+</node>
+</node>
+<node CREATED="1576670554763" ID="ID_285640058" MODIFIED="1576670783474" TEXT="BlocklyJs"/>
+<node CREATED="1576670784826" ID="ID_1056821536" MODIFIED="1576670787657" TEXT="NG"/>
+</node>
+<node CREATED="1576670270634" HGAP="-46" ID="ID_152406026" MODIFIED="1576672751711" POSITION="left" TEXT="apps" VSHIFT="-19">
+<node CREATED="1576670883103" ID="ID_1304572464" MODIFIED="1576671747258" TEXT="BTC Reseller" VSHIFT="2"/>
+<node CREATED="1576670896602" ID="ID_149985484" MODIFIED="1576670905375" TEXT="Asics"/>
+<node CREATED="1576670905908" ID="ID_578753545" MODIFIED="1576672312005" TEXT="Gallary">
+<node CREATED="1576670921381" ID="ID_759958833" MODIFIED="1576670938887" TEXT="Subscriptions"/>
+<node CREATED="1576670939693" ID="ID_1745496663" MODIFIED="1576670946627" TEXT="followup">
+<node CREATED="1576670955105" ID="ID_1091081184" MODIFIED="1576670959130" TEXT="profiles"/>
+</node>
+</node>
+<node CREATED="1576670913918" ID="ID_1245324759" MODIFIED="1576672310652" TEXT="ERP">
+<node CREATED="1576670983384" ID="ID_519723736" MODIFIED="1576671678821" TEXT="Accounting" VSHIFT="25"/>
+<node CREATED="1576670989590" HGAP="17" ID="ID_454330364" MODIFIED="1576672978967" STYLE="bubble" VSHIFT="7">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      queueing
+    </p>
+    <p>
+      and appointments
+    </p>
+    <p>
+      and allocation
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1576671012535" HGAP="23" ID="ID_631057817" MODIFIED="1576673002176" VSHIFT="6">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      clinic
+    </p>
+    <p>
+      management
+    </p>
+    <p>
+      webapp
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1576671030153" ID="ID_1148365801" MODIFIED="1576671034287" TEXT="NetSouq">
+<node CREATED="1576671035460" ID="ID_1963792771" MODIFIED="1576671042276" TEXT="auctioning"/>
+<node CREATED="1576671042644" ID="ID_1013079940" MODIFIED="1576671055139" TEXT="food-delivery system"/>
+<node CREATED="1576671056656" ID="ID_453600107" MODIFIED="1576671063985" TEXT="menus and cats"/>
+</node>
+</node>
+<node CREATED="1576671075391" ID="ID_1210102771" MODIFIED="1576671084296" TEXT="schools model"/>
+<node CREATED="1576671084755" ID="ID_784999501" MODIFIED="1576671097329" TEXT="ProjMan / taskMan"/>
+<node CREATED="1576671097810" ID="ID_139022603" MODIFIED="1576671113310" TEXT="Public Opinion">
+<node CREATED="1576671114390" HGAP="13" ID="ID_1299383682" MODIFIED="1576671793435" TEXT="haycalat ensan" VSHIFT="23"/>
+</node>
+<node CREATED="1576671134351" ID="ID_457570250" MODIFIED="1576672942349" VSHIFT="-3">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      relational
+    </p>
+    <p>
+      calculator
+    </p>
+  </body>
+</html></richcontent>
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Prolog-concepts-visualizations \ mindmapping
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1576671198024" ID="ID_263752834" MODIFIED="1576671215103" TEXT="social network meta-inf"/>
+<node CREATED="1576671215556" ID="ID_1412128197" MODIFIED="1576671288054" TEXT="conversation software">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      TTS / voice recognition \ intelligence mind map \ knowledge management
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1576671294348" ID="ID_1478731116" MODIFIED="1576671301106" TEXT="family tree"/>
+</node>
+<node CREATED="1576670291687" HGAP="25" ID="ID_1247168068" MODIFIED="1576673126906" POSITION="right" TEXT="research" VSHIFT="6">
+<node CREATED="1576670298853" ID="ID_1836723850" MODIFIED="1576670306695" TEXT="Nscript"/>
+<node CREATED="1576670307189" ID="ID_1076579374" MODIFIED="1576670317375" TEXT="Study cgminer"/>
+</node>
+</node>
+</map>
