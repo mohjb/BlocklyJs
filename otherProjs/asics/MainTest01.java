800915c93f1169ce4c0e26d8532272bca008d079
mohjb <mohamadjb@gmail.com>
2020-01-14 15:39:01 +0300

    Update MainTest01.java

--- otherProjs/asics/MainTest01.java
+++ otherProjs/asics/MainTest01.java
@@ -78,76 +78,159 @@ static class Asic extends Json{
 	}
 
 	static class ParseStatus implements Parse{
+		//static P sttcP;
 		@Override public String parse(String s) {
-			P p=new p();p.s=s;
-			p.len=s.length();
-			p.f=p.c=s.charAt(0)=='<'?new E():new N();
-			p.c.i=0;p.c.p();
+			ParseStatus p=new ParseStatus(s);//p.c.parse();
 
 			Map m=new HashMap();
-			p.pTxt(m,0,null);
-			String r=jo().oMap(m);
+			//p.pTxt(m,0,null);
+			String r=Json.jo().oMap(m);
 			return r;
+		}//parse
+
+		ParseStatus(String s){
+			doc=new E(s);
+			 // doc.firstChild=doc.lastChild=doc;
+			int i=s.indexOf('<');
+			doc.firstChild=doc.lastChild=
+				i==0?new E(doc):new N(doc,i);
+			if(i==0)(()c).parse();
+			//if(i>0)new E(doc).parse();
+			while(c.end<doc.end){
+				int j=c.end;
+				i=s.indexOf('<',j);
+				boolean b=i-j<2 &&i>=0;
+				c=b?new E(doc):new N(doc,i);
+				if(b)c.parse();
+			}
 		}
-		static class P {
-			String s;int len;
-			N f,c;//first
-			class N{
-				N parentNode,nextSibling,previousSibling;
-				String x;
-				int i,j;//0:index-of start of textNode ; 1:index-of end-of-textNode 
-				//in-case-of element: 0:index-of '<' ; 1:index-of '>' ; 2:index-of '</' ; 3:index-of '>' ; 
-				void p(){
-					j=s.indexOf("<",i);
-					if(j==-1)j=len;else{
-						
-					}}
+
+		//static class P extends E{
+		E doc;N c;
+
+		class N{
+			N parentNode,nextSibling,previousSibling;
+			String x;int i,end;//i:index-of start of textNode ; end:index-of end-of-textNode
+
+			N(E p){
+				parentNode=p;
+				N n=p.lastChild;
+				i=c.end<doc.end&&doc.x.charAt(c.end)=='>'?c.end+1:c.end;//c.end==0?0:c.end+1;//p.head==0?0:p.head+1;
+				if(n!=null)
+					n.nextSibling=this;
+				previousSibling=n;
+				p.lastChild=this;
+				if(p.firstChild==null)
+					p.firstChild=this;
+				c=this;
 			}
-			class E extends N{
-				N firstChild,lastChild;
-				String id;int ei,ej;
-				void p(){}
-			}
-			void pTxt(Map<K,V> p,int prevSiblingsCount,String id){
-				/*
-					html parser, looking for textNode and sub-tags, and attrib-id in tags
-				*/
-				if(id==null)id=String.valueOf(prevSiblingsCount);
-				prevSiblingsCount=0;
-				int ix=p.indeixOf("<",i);
-				String val=s.substring(i,ix==-1?len:ix);
-				while(ix==-1){
-					p.put(id, val);
-					if(s.charAt(ix+1)=='/'){
-						int j=s.indexOf('>',ix);
-						i=(j==-1?ix:j)+1;
-						return;
-					}
-					else{ i=ix;
-						pTag( p,prevSiblingsCount++);
-						ix=p.indeixOf("<",i);
-						val+=s.substring(i,ix==-1?len:ix);
-					}
-				}p.put(id, val);
+			N(E p,int separator){
+				this(p);if(separator!=-1)
+					x=doc.x.substring(i,separator);
 			}
 
-			Map pTag(Map p,int prevSiblingsCount){
-				int x=i,j=s.indexOf('>',i);
+			N parse(){return this;/*
+				end=doc.x.indexOf("<",i);
+				x=doc.x.substring(i,end!=-1?end:end=doc.end);
+			*/}//parse
+		}//class N
+
+		class E extends N{
+			N firstChild,lastChild;String id;
+			/**i=indexOf('<') head=indexOf('>',i) close=indexOf('<',head) end=indexOf('>',close)*/
+			int head,close;
+			E(String s){
+				c=doc=this;
+				x=s;
+				i=head=0;
+				end=close=(s.length();
+			}
+
+			N parse(){
+				head=close=end=doc.x.indexOf(">",i);
+				if(end==-1)
+					head=close=end=doc.end;
+				else {
+					int e =doc.x.indexOf(" ",i)
+						,d=doc.x.indexOf("id=\"",e)
+						,q=d>end||d==-1?d:doc.x.indexOf("\"",d+4);
+					x=doc.x.substring(i+1,e);
+					id=d==-1?null:doc.x.substring(d+4,q);
+					if(doc.x.charAt(head-1)=='/')
+						close=head-1;           //leaf
+					else{
+						e =doc.x.indexOf('<',head);
+						if(e==-1){
+							if(head<doc.end)
+								new N(this);
+						}else{
+							if(e>head+1)new N(this);
+							if(doc.x.charAt(e+1)=='/'){
+								ei=eend=end+1;
+								e=ei+x.length();
+								if(x.equalsIgnoreCase(doc.x.substring(ei,e))){
+									end=doc.x.indexOf(">",close=end+1);
+
+								}
+							}
+						}
+						/*if(doc.x.charAt(end+1)=='<'){
+							else{
+
+							}
+						}
+						else{
+							firstChild=new N(this);
+							firstChild.parse();
+
+						}*/
+					}
+				}return this;
+			}//parse
+
+		}//class E
+
+		void pTxt(Map p,int prevSiblingsCount,String id){
+			/*
+				html parser, looking for textNode and sub-tags, and attrib-id in tags
+			*/
+			if(id==null)id=String.valueOf(prevSiblingsCount);
+			prevSiblingsCount=0;
+			int ix=p.indeixOf("<",i);
+			String val=doc.x.substring(i,ix==-1?len:ix);
+			while(ix==-1){
+				p.put(id, val);
+				if(doc.x.charAt(ix+1)=='/'){
+					int j=doc.x.indexOf('>',ix);
+					i=(j==-1?ix:j)+1;
+					return;
+				}
+				else{ i=ix;
+					pTag( p,prevSiblingsCount++);
+					ix=p.indeixOf("<",i);
+					val+=doc.x.substring(i,ix==-1?len:ix);
+				}
+			}p.put(id, val);
+		}
+
+		Map pTag(Map p,int prevSiblingsCount){
+				int x=i,j=doc.x.indexOf('>',i);
 				i=(j==-1?len:j)+1;
-				int idx=s.indexOf("id=", x),ide=s.indexOf('"', idx+4);
+				int idx=doc.x.indexOf("id=", x),ide=doc.x.indexOf('"', idx+4);
 				String id=idx==-1
 					?String.valueOf(prevSiblingsCount)
-					:s.substring(idx+4,ide);
+					:doc.x.substring(idx+4,ide);
 				if(idx!=-1){
 
 				}
 				
-				if(j!=-1&&'/'==s.charAt(j-1)){
+				if(j!=-1&&'/'==doc.x.charAt(j-1)){
 					return p;
 				}
 				return p;
 			}
-		}//class P
+
+		//}//class P
 
 	}//class ParseStatus
 
