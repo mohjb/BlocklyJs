b9abcb5636317def2da5934976ae6dd187a4ad9e
mohjb <mohamadjb@gmail.com>
2020-01-15 17:29:14 +0300

    Update MainTest01.java

--- otherProjs/asics/MainTest01.java
+++ otherProjs/asics/MainTest01.java
@@ -1,8 +1,6 @@
 //{1503603665 yd81a3
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.LinkedList;
+import java.io.IOException;
+import java.util.*;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -42,8 +40,9 @@ static void w(String pf,Date now,String fn,String ext,String x){
 	 } catch (Exception e) {
 		error(e,"MainTest01.w",fn,x);}}
 
-public static void main(String[]args)
-{try{baseDir=new File("./output/").getCanonicalPath();
+public static void main(String[]args){
+ try{baseDir=new File("./output/").getCanonicalPath();
+
 	new MainTest01();}catch(Exception e){
 		error(e,"main");}
 }//main 
@@ -54,7 +53,7 @@ MainTest01(){
 	int startPort= 141 ,endPort= 141 ,sleep=5000;
 	w(141+"/",global.now=new Date(),"testW",".html","test");
 	scan=new AsicsScanner(prefix, startPort, endPort, sleep);
-	scan.start();
+	//scan.start();
 }
 
 static class Asic extends Json{
@@ -78,29 +77,27 @@ static class Asic extends Json{
 	}
 
 	static class ParseStatus implements Parse{
-		//static P sttcP;
 		@Override public String parse(String s) {
-			ParseStatus p=new ParseStatus(s);//p.c.parse();
-
-			Map m=new HashMap();
-			//p.pTxt(m,0,null);
-			String r=Json.jo().oMap(m);
+			String r="";try{
+				ParseStatus p=new ParseStatus(s);
+				Map m=p.m(p.doc);
+				r=Json.jo().clrSW().oMap(m,"","").toStrin_();
+			}catch ( IOException x ){}
 			return r;
 		}//parse
 
 		ParseStatus(String s){
+			if(s==null)return;
 			doc=new E(s);
-			 // doc.firstChild=doc.lastChild=doc;
 			int i=s.indexOf('<');
 			doc.firstChild=doc.lastChild=
 				i==0?new E(doc):new N(doc,i);
-			if(i==0)(()c).parse();
-			//if(i>0)new E(doc).parse();
+			if(i==0)c.parse();	//if(i>0)new E(doc).parse();
 			while(c.end<doc.end){
 				int j=c.end;
 				i=s.indexOf('<',j);
 				boolean b=i-j<2 &&i>=0;
-				c=b?new E(doc):new N(doc,i);
+				c=b?new E(doc):new N(doc,j,i);
 				if(b)c.parse();
 			}
 		}
@@ -112,6 +109,7 @@ static class Asic extends Json{
 			N parentNode,nextSibling,previousSibling;
 			String x;int i,end;//i:index-of start of textNode ; end:index-of end-of-textNode
 
+			N(){c=this;}
 			N(E p){
 				parentNode=p;
 				N n=p.lastChild;
@@ -128,22 +126,24 @@ static class Asic extends Json{
 				this(p);if(separator!=-1)
 					x=doc.x.substring(i,separator);
 			}
+			N(E p,int start,int separator){
+				this(p);i=start;end=separator;if(separator!=-1)
+					x=doc.x.substring(i,separator);
+			}
 
-			N parse(){return this;/*
-				end=doc.x.indexOf("<",i);
-				x=doc.x.substring(i,end!=-1?end:end=doc.end);
-			*/}//parse
+			N parse(){return this;}//parse
 		}//class N
 
 		class E extends N{
 			N firstChild,lastChild;String id;
 			/**i=indexOf('<') head=indexOf('>',i) close=indexOf('<',head) end=indexOf('>',close)*/
 			int head,close;
-			E(String s){
+			E(E p){super(p);}
+			E(String s){super();
 				c=doc=this;
 				x=s;
 				i=head=0;
-				end=close=(s.length();
+				end=close=s.length();
 			}
 
 			N parse(){
@@ -156,87 +156,59 @@ static class Asic extends Json{
 						,q=d>end||d==-1?d:doc.x.indexOf("\"",d+4);
 					x=doc.x.substring(i+1,e);
 					id=d==-1?null:doc.x.substring(d+4,q);
+					e=head;
 					if(doc.x.charAt(head-1)=='/')
 						close=head-1;           //leaf
-					else{
-						e =doc.x.indexOf('<',head);
+					else do{
+						e =doc.x.indexOf('<',d=e);
 						if(e==-1){
-							if(head<doc.end)
-								new N(this);
-						}else{
-							if(e>head+1)new N(this);
-							if(doc.x.charAt(e+1)=='/'){
-								ei=eend=end+1;
-								e=ei+x.length();
-								if(x.equalsIgnoreCase(doc.x.substring(ei,e))){
-									end=doc.x.indexOf(">",close=end+1);
-
+							if(head<(end=doc.end))
+								new N(this,d,doc.end);
+						}else {
+							if ( e > d+1 )//head + 1
+								new N( this ,e);
+							if ( doc.x.charAt( e + 1 ) == '/' ) {
+								end = close = e;
+								c = this;
+								e += d = x.length();
+								if ( x.equalsIgnoreCase( doc.x.substring( end + 2, e ) ) ) {
+									end = doc.x.indexOf( ">", e );
+									if ( end == -1 )
+										end = doc.end;
+									return this;
 								}
+							}else{
+								E x=new E(this);
+								x.parse();
+								e=x.end;
 							}
 						}
-						/*if(doc.x.charAt(end+1)=='<'){
-							else{
-
-							}
-						}
-						else{
-							firstChild=new N(this);
-							firstChild.parse();
-
-						}*/
-					}
+					}while(e!=-1 );
 				}return this;
 			}//parse
 
 		}//class E
 
-		void pTxt(Map p,int prevSiblingsCount,String id){
-			/*
-				html parser, looking for textNode and sub-tags, and attrib-id in tags
-			*/
-			if(id==null)id=String.valueOf(prevSiblingsCount);
-			prevSiblingsCount=0;
-			int ix=p.indeixOf("<",i);
-			String val=doc.x.substring(i,ix==-1?len:ix);
-			while(ix==-1){
-				p.put(id, val);
-				if(doc.x.charAt(ix+1)=='/'){
-					int j=doc.x.indexOf('>',ix);
-					i=(j==-1?ix:j)+1;
-					return;
-				}
-				else{ i=ix;
-					pTag( p,prevSiblingsCount++);
-					ix=p.indeixOf("<",i);
-					val+=doc.x.substring(i,ix==-1?len:ix);
-				}
-			}p.put(id, val);
-		}
-
-		Map pTag(Map p,int prevSiblingsCount){
-				int x=i,j=doc.x.indexOf('>',i);
-				i=(j==-1?len:j)+1;
-				int idx=doc.x.indexOf("id=", x),ide=doc.x.indexOf('"', idx+4);
-				String id=idx==-1
-					?String.valueOf(prevSiblingsCount)
-					:doc.x.substring(idx+4,ide);
-				if(idx!=-1){
-
-				}
-				
-				if(j!=-1&&'/'==doc.x.charAt(j-1)){
-					return p;
-				}
-				return p;
-			}
-
 		//}//class P
 
+		Map m(E e){Map m =new HashMap();
+			N n=e.firstChild;
+			int i=0;
+			while(n!=null){
+				if(n instanceof E){
+					E x=(E)n;
+					m.put( x.id==null?i:x.id,m(x) );
+				}
+				else m.put(i,n.x);
+				n=n.nextSibling;
+				i++;
+			}
+			return m;}
 	}//class ParseStatus
 
 	static Parse parse0=new Parse0()
 		,parseConfig=new ParseConfig()
-		,parseStatus=new ParseStatus();
+		,parseStatus=new ParseStatus(null);
 
 	enum State{init,detectAsic,invalidWallet,wallet,config,monitor};
 	enum Path{info("/cgi-bin/get_system_info.cgi",parse0)
@@ -261,7 +233,7 @@ static class Asic extends Json{
 			sb.append(charArray, 0, numCharsRead);
 		String result = sb.toString();
 		//TODO: implement parsing Path.info result string
-		w(ip+"/",global.now,p.toString(),".html",p.parse(result));
+		w(ip+"/",global.now,p.toString(),".html",p.parse.parse(result));
 	}
 
 	public void startScan()throws Exception{
